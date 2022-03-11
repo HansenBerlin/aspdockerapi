06445488c4b83d1e89aa6c54f26a2238db2d0f7c
LT-6FGD373\Hannes <h.roever@zertificon.com>
2022-03-11 12:51:57 +0100

    init

---
+++ Controllers/PrimeNumberCheckerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspdockerapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PrimeNumberCheckerController : ControllerBase
+{
+    private readonly ILogger<PrimeNumberCheckerController> _logger;
+
+    public PrimeNumberCheckerController(ILogger<PrimeNumberCheckerController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("{checkNumber}")]
+    public PrimeState Get(int checkNumber)
+    {
+        if (checkNumber == 1)
+            return PrimeState.IsNoPrime;
+        if (checkNumber <= 0)
+            return PrimeState.Invalid;
+        
+        PrimeState state = PrimeState.IsPrime;
+        for (int i = 2; i < checkNumber+1; i++)
+        {
+            if (checkNumber % i == 0 && checkNumber != i)
+            {
+                state = PrimeState.IsNoPrime;
+            }
+        }
+        return state;
+    }
+}
---
+++ PrimeState.cs
@@ -0,0 +1,8 @@
+﻿namespace aspdockerapi;
+
+public enum PrimeState
+{
+    IsPrime,
+    IsNoPrime,
+    Invalid
+}
